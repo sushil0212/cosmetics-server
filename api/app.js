8a0274292ad5b549bdb502c776744effdf576ad1
“Sushil <sushilstar9@gmail.com>
2024-06-29 14:30:15 +0100

    Done

---
+++ api/app.js
@@ -0,0 +1,35 @@
+const jsonServer = require('json-server');
+const morgan = require('morgan');
+
+const server = jsonServer.create();
+
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join('db.json');
+const data = fs.readFileSync(filePath, 'utf-8');
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(morgan('dev'));
+
+server.use((req, res, next) => {
+  // Middleware to disable CORS
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1'
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
+// Export the Server API
+module.exports = server;
